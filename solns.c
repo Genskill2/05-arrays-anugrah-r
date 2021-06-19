acb9fc8dd95ea8c06c22cdec021dbf654446512b
anugrah-r <84712467+anugrah-r@users.noreply.github.com>
2021-06-19 10:46:11 +0530

    Update solns.c

--- solns.c
+++ solns.c
@@ -1,2 +1,15 @@
 /* Enter your solutions in this file */
 #include <stdio.h>
+
+int max(int a[], int n)
+{
+    int maxn = 0;
+    for(int i = 0; i<n; i++)
+    {
+        if (a[i] > maxn)
+            maxn = a[i];
+    }
+    return maxn;
+}
+
+
